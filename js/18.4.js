d36247b29bf545876c1c370df3037685e3218555
artkar17 <101562847+artkar17@users.noreply.github.com>
2022-11-21 15:28:42 +0300

    Create 18.4.js

---
+++ js/18.4.js
@@ -0,0 +1,6 @@
+let a = /[jpg]$/.test('eft.jpg wdwr.jpeg');
+console.log(a)
+let b = /[jpeg]$/.test('wdwr.jpeg eft.jpg');
+console.log(b)
+let c = /[jpg]$/.test('eft.jpg wdwr.jpeg');
+console.log(c)
